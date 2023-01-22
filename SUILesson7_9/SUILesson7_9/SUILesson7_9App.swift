a744d37bd14f8479fd9148aa0b584abb55806ca6
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2023-01-23 00:19:31 +0500

    initCommit

---
+++ SUILesson7_9/SUILesson7_9/SUILesson7_9App.swift
@@ -0,0 +1,17 @@
+//
+//  SUILesson7_9App.swift
+//  SUILesson7_9
+//
+//  Created by Григоренко Александр Игоревич on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUILesson7_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
